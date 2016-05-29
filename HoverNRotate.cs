f8e85a9c85852880c3e9c373681feb705b3f5398
keyeno33 <robert.299@hotmail.com>
2016-05-29 18:12:43 -0400

    UTYScripsAsset

---
+++ HoverNRotate.cs
@@ -0,0 +1,18 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class ObjectHover : MonoBehaviour {
+    public Rigidbody rb;
+    void start()
+    {
+        rb = GetComponent<Rigidbody>();
+    }
+    void hover()
+    {
+
+    }
+    void rotate()
+    {
+
+    }
+}
---
+++ PlayerController.cs
@@ -0,0 +1,66 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class PlayerController : MonoBehaviour
+{
+    public int speed;
+
+    private Rigidbody rb;
+
+    // Use this for initialization
+    void Start()
+    {
+        rb = GetComponent<Rigidbody>();
+    }
+
+    // Update is called once per frame
+    void FixedUpdate()
+    {
+        //Get input for each axis;
+        float moveHorizontal = Input.GetAxis("Horizontal");
+        float moverVertical = Input.GetAxis("Vertical");
+
+        Vector3 movement = new Vector3(moveHorizontal, 0.0f, moverVertical);
+        rb.AddForce(movement * speed);
+
+
+    }
+
+    void OnCollisionEnter(Collision col)
+    {
+        Destroy(col.gameObject);
+        //function call for functional abjects
+
+        switch (col.gameObject.name)
+        {
+            case "Speed_Cube":
+                speed = 20;
+                break;
+
+            case "Dec_Speed_Cube":
+                speed -= 5;
+                break;
+
+            case "Scale_Inc":
+                transform.localScale += new Vector3(.2f, .2f, .2f);
+                break;
+
+            case "Scale_Dec":
+                transform.localScale += new Vector3(.2f, .2f, .2f);
+                break;
+
+            case "PlayerMultiplier":
+                GameObject childPlayer;
+                childPlayer = new GameObject("player");
+                childPlayer.AddComponent<Rigidbody>();
+                childPlayer.AddComponent<BoxCollider>();
+                break;
+
+            default:
+                return;
+                break;
+        }
+
+    }
+}
+
