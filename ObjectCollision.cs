837f312b8252ac3e777aa7e0daac00b441b5a0b9
keyeno33 <robert.299@hotmail.com>
2016-05-29 20:19:08 -0400

    Object Script (edit)

--- ObjectCollision.cs
+++ ObjectCollision.cs
@@ -3,7 +3,7 @@ using System.Collections;
 
 public class ObjectCollision : MonoBehaviour {
 
-    public int speed;
+    public int Speed;
 
     void OnCollisionEnter(Collision col)
     {
@@ -12,34 +12,46 @@ public class ObjectCollision : MonoBehaviour {
 
         switch (col.gameObject.name)
         {
-            case "Quick_Speed_Cube": //Increases playerObject speed by 20
+            case "Cube":
                 Destroy(col.gameObject);
-                speed += 20;
                 break;
 
-            case "speed_Cube": //Increases playerObject speed by 5
+            case "Speed_Cube": //Increases playerObject speed by 5
+                Speed += 5;
                 Destroy(col.gameObject);
-                speed += 5;
                 break;
 
             case "Dec_Speed_Cube": //Decreases playerObject speed
+                Speed -= 5;
                 Destroy(col.gameObject);
-                speed -= 5;
+                break;
+
+            case "Default_Speed":
+                Speed = 5;
+                Destroy(col.gameObject);
                 break;
 
             case "Scale_Inc": //Icreases playerObject transform Scale property
-                transform.localScale += new Vector3(.2f, .2f, .2f);
+                transform.localScale += new Vector3(2f, 2f, 2f);
+                Destroy(col.gameObject);
                 break;
 
             case "Scale_Dec": // Decreases playerObject transform Scale property
-                transform.localScale -= new Vector3(.2f, .2f, .2f);
+                transform.localScale = new Vector3(.5f, .5f, .5f);
+                Destroy(col.gameObject);
                 break;
 
+            case "Default_Scale":
+                transform.localScale = new Vector3(1f, 1f, 1f);
+                Destroy(col.gameObject);
+                break;
+                 
             case "PlayerMultiplier": // adds another playerObject 
                 GameObject childPlayer;
                 childPlayer = new GameObject("player"); // New player initialization 
                 childPlayer.AddComponent<Rigidbody>(); // Add Rigidbody Component
                 childPlayer.AddComponent<BoxCollider>();//Add BoxCollider Component
+                Destroy(col.gameObject);
                 break;
 
             default:
---
+++ ObjectDestroy.cs
@@ -0,0 +1,10 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class ObjectDestroy : MonoBehaviour {
+
+    void onCollisionEnter(Collision col)
+    {
+        Destroy(col.gameObject);
+    }
+}
